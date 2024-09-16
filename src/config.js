47c72ce1bebf85bc70160a12603c9cd6609503ce
Javier Morales <jmoralesm45@miumg.edu.gt>
2024-09-15 19:24:26 -0600

    Se guardan las imagenes de forma local por el momento

--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@ module.exports = {
         port: process.env.PORT || 3000
     },
     db: {
-        host: process.env.DB_HOST || 'localhost',
+        host: process.env.DB_HOST || '195.250.27.25',
         user: process.env.DB_USER || 'root',
         password: process.env.DB_PASSWORD || 'Javier123.',
         database: process.env.DB_NAME || 'db_restapi'
