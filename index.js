f014d705073b897878079743ac316b23e402b92a
Javier Morales <jmoralesm45@miumg.edu.gt>
2024-09-18 16:15:42 -0600

    Cambios a raiz de las imagenes

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 const app = require('./src/app');
 const config = require('./src/config');
 
-// Definir el prefijo de ruta base
-const BASE_PATH = '/api.asambleasdedios.gt/api/asambleas';
-
 // Usar el prefijo de ruta para todas las rutas
-app.use(BASE_PATH, require('./src/modulos/asambleas/rutas'));
+app.use('/api.asambleasdedios.gt/api/asambleas', require('./src/modulos/asambleas/rutas'));
 
 app.listen(app.get('port'), '0.0.0.0', () => {
     console.log(`Servidor ejecutándose en el puerto ${config.app.port}`);
