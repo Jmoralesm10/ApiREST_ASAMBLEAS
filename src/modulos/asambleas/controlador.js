baf097bbd7c744bf73312e623e1be55b2da4528d
Javier Morales <jmoralesm45@miumg.edu.gt>
2024-09-18 15:59:13 -0600

    Cambios para imagenes

--- src/modulos/asambleas/controlador.js
+++ src/modulos/asambleas/controlador.js
@@ -152,7 +152,7 @@ const buscarIglesias = async (req, res) => {
 
                 return {
                     ...iglesia,
-                    foto_perfil: iglesia.foto_perfil ? `/api.asambleasdedios.gt/imagenes/iglesias/${iglesia.foto_perfil}` : null,
+                    fotoPerfil: iglesia.fotoPerfil ? `/api.asambleasdedios.gt/imagenes/iglesias/${iglesia.fotoPerfil}` : null,
                     redes_sociales: redesSociales,
                     horarios_servicios: horariosServicios
                 };
