11cab9107b990cfd3b09c8c91f033e7dbcee43b7
Javier Morales <jmoralesm45@miumg.edu.gt>
2024-09-18 18:12:05 -0600

    Cambios para cargar fotos de perfil

--- src/app.js
+++ src/app.js
@@ -22,6 +22,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api.asambleasdedios.gt/api/asambleas', asambleasRoutes);
 
 // SERVIR IMAGENES ESTÁTICAMENTE
-app.use('/api.asambleasdedios.gt/imagenes', express.static(path.join(__dirname, '..', 'API_REST', 'imagenes')));
+app.use('/api.asambleasdedios.gt/imagenes', express.static(path.join(__dirname, '..', 'imagenes')));
 
 module.exports = app;
